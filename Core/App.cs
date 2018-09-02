d69963b93c279debcbe54bbf7444c91ccc7335d4
ChristianFarias23 <kurisu615@gmail.com>
2018-09-02 05:19:37 -0300

    Creacion de la clase Cotizacion.

--- Core/App.cs
+++ Core/App.cs
@@ -17,9 +17,10 @@ namespace Core
         /// <exception cref="ModelException"></exception>
         private static void Main(string[] args)
         {
-            Console.WriteLine("Building Sistema ..");
+            Console.WriteLine("Iniciando la aplicacion...");
             ISistema sistema = Startup.BuildSistema();
 
+            /* Codigo de ejemplo:
             Console.WriteLine("Creating Persona ..");
             {
                 Persona persona = new Persona()
@@ -48,8 +49,61 @@ namespace Core
                     Console.WriteLine("Persona = " + Utils.ToJson(persona));
                 }
             }
+            */
 
-            Console.WriteLine("Done.");
+            // crear servicios, crear cotizacion.
+            {
+                //Crear persona (Cliente).
+
+                Persona persona = new Persona()
+                {
+                    Rut = "130144918",
+                    Nombre = "Diego",
+                    Paterno = "Urrutia",
+                    Materno = "Astorga",
+                    Email = "durrutia@ucn.cl"
+                };
+                
+                //Crear cotizacion.
+                
+                Cotizacion cotizacion = new Cotizacion()
+                {
+                    RutCliente = persona.Rut,
+                    Titulo = "Video resumen 10 años Cemp UCN",
+                    Descripcion = "Grabación, edición y postproducción de video de 3 a 4 minutos " +
+                                  "sobre actividad de los 10 años del Cemp UCN. El valor incluye 2 revisiones previa entrega, postproducción " +
+                                  "de imagen y audio, gráficas de presentación inicio y cierre. Los valores por este trabajo " +
+                                  "son de acuerdo a tarifa especial para unidades pertenecientes a la UCN",
+                    fechaCreacion = DateTime.Now,
+
+                };
+                
+                //Crear los servicios.
+                
+                int servicio1 = 10000;
+                int servicio2 = 12000;
+                int servicio3 = 9000;
+                int servicio4 = 15000;
+
+                IList<int> servicios = new List<int>();
+                
+                servicios.Add(servicio1);
+                servicios.Add(servicio2);
+                servicios.Add(servicio3);
+                servicios.Add(servicio4);
+
+                //Asignar los servicios a la cotizacion.
+                
+                cotizacion.AsignarServicios(servicios);
+                
+                //sistema.Save();
+                
+                Console.WriteLine(Utils.ToJson(persona));
+                Console.WriteLine(Utils.ToJson(cotizacion));
+
+            }
+
+            Console.WriteLine("Fin de la aplicacion.");
         }
     }
 }
---
+++ Core/Models/Cotizacion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public class Cotizacion : BaseEntity
+    {
+        /// <summary>
+        /// Identificador de la cotizacion.
+        /// </summary>
+        public string Identificador { get; set; }
+
+        /// <summary>
+        /// Version de la cotizacion.
+        /// </summary>
+        public string Version { get; set; }
+        
+        /// <summary>
+        /// Titulo de la cotizacion.
+        /// </summary>
+        public string Titulo { get; set; }
+        
+        /// <summary>
+        /// Descripcion breve del contexto de la cotizacion.
+        /// </summary>
+        public string Descripcion { get; set; }
+
+        /// <summary>
+        /// Fecha de creacion de esta cotizacion.
+        /// </summary>
+        public DateTime fechaCreacion { get; set; }
+
+        /// <summary>
+        /// Rut del cliente al cual le es asignada esta cotizacion.
+        /// </summary>
+        public string RutCliente { get; set; }
+
+        //public IList<Servicio> Servicios {get; set;}
+        
+        /// <summary>
+        /// Costo total del la cotizacion.
+        /// </summary>
+        public int CostoTotal { get; set; }
+        
+        /// <summary>
+        /// Estado en el cual se encuentra la cotizacion.
+        /// </summary>
+        public EstadoCotizacion Estado { get; set; }
+
+
+        /// <summary>
+        /// Calcula y asigna el costo total de esta cotizacion.
+        /// </summary>
+        /// <param name="costosServicios"></param>
+        public void AsignarServicios(IList<int> servicios)
+        {
+            this.CostoTotal = 0;
+
+            foreach (int servicio in servicios)
+            {
+                //servicio.idCotizacion = this.identificador;
+                this.CostoTotal += servicio;
+            }
+        }
+
+
+        /// <inheritdoc cref="BaseEntity.Validate"/>
+        public override void Validate()
+        {
+            if (String.IsNullOrEmpty(Version))
+            {
+                throw new ModelException("Version no puede ser null");
+            }
+            
+            if (String.IsNullOrEmpty(Titulo))
+            {
+                throw new ModelException("El titulo no puede estar vacio.");
+            }
+
+            if (String.IsNullOrEmpty(Descripcion))
+            {
+                throw new ModelException("La descripcion no puede estar vacia.");
+            }
+            
+        }
+    }
+    
+    public enum EstadoCotizacion
+    {
+        Borrador,
+        Enviada,
+        Aprobada,
+        Rechazada,
+        Terminada
+    };
+
+}
